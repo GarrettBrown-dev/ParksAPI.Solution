a472bdd31a8d157f68a85b3ff57d9f8d3eedf7fb
Garrett <gman9mm@live.com>
2021-01-22 10:17:04 -0800

    Building out controller.

---
+++ ParksAPI/Controllers/ParksController.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using System.Linq;
+using ParksAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ParksAPI.Controllers
+{
+    [Route("api/parks")]
+    [ApiController]
+    public class ParksController : ControllerBase
+    {
+        private ParksAPIContext _db;
+        public ParksController(ParksAPIContext db)
+        {
+            _db = db;
+        }
+
+        //GET api/parks
+        [HttpGet]
+        public ActionResult<IEnumerable<Park>> GetAction(string parkLocation, string parkName, string description)
+        {
+            var query = _db.Parks.AsQueryable();
+
+            if (parkLocation != null)
+            {
+                query = query.Where(entry => entry.ParkLocation == parkLocation);
+            }
+
+            if (parkName != null)
+            {
+                query = query.Where(entry => entry.ParkName == parkName);
+            }
+
+            if (description != null)
+            {
+                query = query.Where(entry => entry.Description == description);
+            }
+            return query.ToList();
+        }
+
+        // POST api/parks
+        [HttpPost]
+        public void Post([FromBody] Park park)
+        {
+            _db.Parks.Add(park);
+            _db.SaveChanges();
+        }
+
+        //GET api/parks
+        [HttpGet("{id}")]
+        public ActionResult<Park> Get(int id)
+        {
+            return _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+        }
+
+        //PUT api/park/3
+
